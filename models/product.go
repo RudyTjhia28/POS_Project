969123d403485fff543dd9b0f4ae6c0414346bb6
rudytjhia28 <rudytjhia28@gmail.com>
2023-04-14 15:59:20 +0700

    remove ID from struct as for ID already covered by gorm model

--- models/product.go
+++ models/product.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 // Product model
 type Product struct {
 	gorm.Model
-	Id       string  `json:"id"`
+	// Id       string  `json:"id"`
 	Name     string  `json:"name"`
 	Price    float64 `json:"price"`
 	Quantity uint    `json:"quantity"`
