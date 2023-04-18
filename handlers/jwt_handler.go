68f9d91a76cdc7a563b0fa1f8486810ffb84f5e5
rudytjhia28 <rudytjhia28@gmail.com>
2023-04-18 11:09:55 +0700

    uncapitalize error string

--- handlers/jwt_handler.go
+++ handlers/jwt_handler.go
@@ -22,7 +22,7 @@ func JWTMiddleware() gin.HandlerFunc {
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 			}
 
 			//TO-DO
