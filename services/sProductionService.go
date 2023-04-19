e1a4a97ba76320f0dad7cbb7700c31ed52836fc4
rudytjhia28 <rudytjhia28@gmail.com>
2023-04-19 17:04:27 +0700

    fix bug where service not calling repo and route the productService private function to use repository function

--- services/sProductionService.go
+++ services/sProductionService.go
@@ -3,8 +3,8 @@ package services
 import "pos_project/models"
 
 type ProductService interface {
-	GetProducts() ([]models.Product, error)
-	GetProductById(id string) (models.Product, error)
+	GetProducts() (*[]models.Product, error)
+	GetProductById(id string) (*models.Product, error)
 	CreateProduct(*models.CreateProductRequest) (*models.Product, error)
 	UpdateProduct(*models.CreateProductRequest) (*models.Product, error)
 }
--- services/sProductionServiceImpl.go
+++ services/sProductionServiceImpl.go
@@ -1,56 +1,52 @@
 package services
 
 import (
-	"errors"
 	"pos_project/models"
+	"pos_project/repositories"
 
 	"gorm.io/gorm"
 )
 
 type productService struct {
-	db *gorm.DB
+	productRepo *repositories.ProductRepository
 }
 
 func NewProductService(db *gorm.DB) *productService {
-	return &productService{db}
+	productRepo := repositories.NewProductRepository(db)
+	return &productService{productRepo}
 }
 
-func (ps *productService) GetProducts() ([]models.Product, error) {
-	var products []models.Product
-	if err := ps.db.Find(&products).Error; err != nil {
+func (ps *productService) GetProducts() (*[]models.Product, error) {
+	products, err := ps.productRepo.GetProducts()
+	if err != nil {
 		return nil, err
 	}
 
-	return products, nil
+	return &products, nil
 }
 
-func (ps *productService) GetProductById(id string) (models.Product, error) {
-	var product models.Product
-	if err := ps.db.First(&product, id).Error; err != nil {
-		return models.Product{}, errors.New("record not found")
+func (ps *productService) GetProductById(id string) (*models.Product, error) {
+	product, err := ps.productRepo.GetProductById(id)
+	if err != nil {
+		return nil, err
 	}
-
-	return product, nil
+	return &product, nil
 }
 
 func (ps *productService) CreateProduct(input *models.CreateProductRequest) (*models.Product, error) {
 	product := models.Product{Name: input.Name, Price: input.Price, Quantity: input.Quantity}
-	if err := ps.db.Create(&product).Error; err != nil {
+	result, err := ps.productRepo.CreateProduct(models.CreateProductRequest(product))
+	if err != nil {
 		return nil, err
 	}
 
-	return &product, nil
+	return &result, nil
 }
 
 func (ps *productService) UpdateProduct(input *models.CreateProductRequest) (*models.Product, error) {
-	var oldProduct models.Product
-	if err := ps.db.First(&oldProduct, input.ID).Error; err != nil {
-		return nil, errors.New("record not found")
-	}
-
-	if err := ps.db.Model(&oldProduct).Updates(input).Error; err != nil {
+	result, err := ps.productRepo.UpdateProduct(*input)
+	if err != nil {
 		return nil, err
 	}
-
-	return &oldProduct, nil
+	return &result, nil
 }
